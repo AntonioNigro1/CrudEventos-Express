fe8f33f45c9ee837ccdac2107d9064c56140af9f
AntonioNigro1 <46200602+AntonioNigro1@users.noreply.github.com>
2022-07-06 14:23:09 -0300

    Funções de Evento
    Adicionado controlador com as funções add, find, delete, show all para eventos

---
+++ app/controllers/EventController.js
@@ -0,0 +1,63 @@
+const Evento = require('../models/Evento');
+
+module.exports = {
+  add: async (req, res) => {
+    const { nome, data, tempo } = req.body;
+    try {
+      // Create new Evento
+      const reply = await Evento.create({
+        nome,
+        data,
+        tempo
+      });
+      res.status(200).json({ status: '200' });
+    } catch (err) {
+      console.log(err);
+      res.status(401).json({ status: '401', error: '401 Not Authenticaded' });
+    }
+  },
+  delete: async (req, res) => {
+    const { nome, data } = req.body;
+    try {
+      // Search User
+      const reply = await deleteOne(Evento.find({ nome: nome, data: data }).);
+      console.log(reply);
+      if (reply.deletedCount == 1) {
+        res.status(200).json({ status: '200' });
+      } else {
+        res.status(404).json({ status: '404', error: '404 Event Not Found' });
+      }
+    } catch (err) {
+      console.log(err);
+      res.status(401).json({ status: '401', error: '401 Not Authenticaded' });
+    }
+  },
+  showAll: async (req, res) => {
+    const { nome, data, tempo } = req.body;
+    try {
+      const reply = await Evento.find({}).toArray();
+      if (reply != null) {
+        console.log(reply);
+        res.status(200).json({ status: '200', data: reply });
+      } else {
+        res.status(404).json({ status: '404', error: 'No Events Found' });
+      }
+    } catch (error) {
+      res.status(401).json({ status: '401', error: '401 Not Authenticaded' });
+    }
+  },
+  findIt: async (req, res) => {
+    const { nome, data, tempo } = req.body;
+    try {
+      const reply = await Evento.findOne({ nome: nome, data: data });
+      if (reply != null) {
+        res.status(200).json({ status: '200', data: reply });
+      } else {
+        res.status(404).json({ status: '404', error: 'Event Not Found' });
+      }
+
+    } catch (error) {
+      res.status(401).json({ status: '401', error: '401 Not Authenticaded' });
+    }
+  }
+}
