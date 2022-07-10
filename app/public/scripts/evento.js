4b9ac3f503372cb52a895a84ffe35cf99fdfbf9b
AntonioNigro1 <46200602+AntonioNigro1@users.noreply.github.com>
2022-07-10 13:38:02 -0300

    Update evento.js

--- app/public/scripts/evento.js
+++ app/public/scripts/evento.js
@@ -42,7 +42,7 @@ enviar.addEventListener('click', async function (event_nome, event_date, event_d
     document.querySelector('#erro').classList.toggle('displaynone', false);
   }
   else if (optionControl == 3) {//procurar
-    const json = await fetch('https://peaceful-ridge-61933.herokuapp.com/Events/find' + event_nome.value);
+    const reply = await fetch('https://peaceful-ridge-61933.herokuapp.com/Events/find' + event_nome.value);
     let obj = await reply.json();
     console.log(obj);
     if (obj.status == 200) {
