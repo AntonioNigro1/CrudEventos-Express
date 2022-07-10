26a188d6cf75a41837a813db3562d2e473a2245c
AntonioNigro1 <46200602+AntonioNigro1@users.noreply.github.com>
2022-07-10 14:10:11 -0300

    Update evento.js

--- app/public/scripts/evento.js
+++ app/public/scripts/evento.js
@@ -78,7 +78,7 @@ verTodos.addEventListener('click', async function (event_nome, event_date, event
   if (obj.status == 200) {
     let events = document.createElement("ol");
     for (let i = 0; i < obj.data.length; i++) {
-      let box = document.createElement("div");
+      let box[] = document.createElement("div");
       let nome = document.createElement("p");
       nome.innerHTML = "Titulo: " + obj.data[i].nome;
       let data = document.createElement("p");
@@ -88,8 +88,8 @@ verTodos.addEventListener('click', async function (event_nome, event_date, event
       data.appendChild(tempo);
       nome.appendChild(data);
       events.appendChild(nome);
-      box.appendChild(events);
-      response.appendChild(box);
+      box[i].appendChild(events);
+      response.appendChild(box[i]);
     }
 
   } else {
